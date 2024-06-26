aaf63d69e59915d4e56c21a75d0851d37d8cbc6e
iamJuao1010 <jaoluizfrancisco@gmail.com>
2024-06-26 09:49:13 -0300

    Segundo commit JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -23,6 +24,18 @@ function receberQuantidadePessoas(evento) {
         paragrafoErro.style.display = "none"
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
-
     }
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao =>{ //"ForEach" = No caso ele vai passar por cada um dos botões
+    botao.addEventListener("click", receberPorcentagem)
+
+})
+
+function receberPorcentagem(evento){
+    console.log(evento.target.value)
+    
+    //porcentagem = Number(evento.target.value) / 100
+    //console.log(porcentagem)
 }
